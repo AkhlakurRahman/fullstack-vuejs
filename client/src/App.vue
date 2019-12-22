e032b8ee9e08159db8622c62dcacae8487414e5c
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-22 23:58:31 +0600

    query photos and styling in frontend completed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,50 +1,50 @@
 <template>
-	<div id="app">
-		<div class="nav">
-			<div class="nav__logo">
-				<router-link to="/">Image Share</router-link>
-			</div>
-			<div class="nav__search--bar">
-				<input type="text" name="search" placeholder="Search images" />
-			</div>
-			<div class="nav__link--items">
-				<router-link to="/">Gallery</router-link>
-				<router-link to="/signin">Sign In</router-link>
-				<router-link to="/signup">Sign Up</router-link>
-			</div>
-		</div>
+  <div id="app">
+    <div class="nav">
+      <div class="nav__logo">
+        <router-link to="/">Image Share</router-link>
+      </div>
+      <div class="nav__search--bar">
+        <input type="text" name="search" placeholder="Search images" />
+      </div>
+      <div class="nav__link--items">
+        <router-link to="/">Gallery</router-link>
+        <router-link to="/signin">Sign In</router-link>
+        <router-link to="/signup">Sign Up</router-link>
+      </div>
+    </div>
 
-		<main>
-			<router-view />
-		</main>
-	</div>
+    <main class="container">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <style lang="scss">
 *,
 *:before,
 *:after {
-	box-sizing: inherit;
-	margin: 0;
-	padding: 0;
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
 }
 
 html {
-	box-sizing: border-box;
-	font-size: 62.5%;
+  box-sizing: border-box;
+  font-size: 62.5%;
 }
 
 body {
-	font-family: 'Fira Sans', arial, sans-serif;
-	font-weight: 300;
-	line-height: 1.6;
-	margin: 0 auto;
+  font-family: "Fira Sans", arial, sans-serif;
+  font-weight: 300;
+  line-height: 1.6;
+  margin: 0 auto;
 }
 
 a {
-	font-weight: 400;
-	color: #fff;
-	text-decoration: none;
+  font-weight: 400;
+  color: #fff;
+  text-decoration: none;
 }
 
 h1,
@@ -53,72 +53,64 @@ h3,
 h4,
 h5,
 h6 {
-	color: rgba(0, 0, 0, 0.84);
+  color: rgba(0, 0, 0, 0.84);
 }
 
 .nav {
-	width: 100vw;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	background-color: #42cafd;
-	height: 10vh;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #42cafd;
+  height: 10vh;
 
-	&__logo {
-		font-size: 2rem;
-		margin-left: 2rem;
-	}
+  &__logo {
+    font-size: 2rem;
+    margin-left: 2rem;
+  }
 
-	&__search--bar {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex: 0 0 30%;
+  &__search--bar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 0 0 30%;
 
-		input {
-			font-family: inherit;
-			border: none;
-			padding: 0.7rem 2rem;
-			border-radius: 3px;
-			width: 90%;
-			transition: all 0.2s;
+    input {
+      font-family: inherit;
+      border: none;
+      padding: 0.7rem 2rem;
+      border-radius: 3px;
+      width: 90%;
+      transition: all 0.2s;
 
-			&:focus {
-				width: 100%;
-				outline: none;
-				background-color: rgb(240, 229, 229);
-			}
-		}
-	}
-
-	&__link--items {
-		a {
-			font-size: 1.6rem;
-			padding: 1rem 2rem;
-			transition: all 0.2s;
-			border-radius: 3px;
-
-			&:hover {
-				background-color: #37a6d0;
-			}
-
-			&.router-link-exact-active {
-				background-color: #37a6d0;
-			}
-		}
-	}
-}
-
-/* #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+      &:focus {
+        width: 100%;
+        outline: none;
+        background-color: rgb(240, 229, 229);
+      }
     }
   }
-} */
+
+  &__link--items {
+    a {
+      font-size: 1.6rem;
+      padding: 1rem 2rem;
+      transition: all 0.2s;
+      border-radius: 3px;
+
+      &:hover {
+        background-color: #37a6d0;
+      }
+
+      &.router-link-exact-active {
+        background-color: #37a6d0;
+      }
+    }
+  }
+}
+
+.container {
+  max-width: 60vw;
+  margin: 0 auto;
+}
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,73 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div v-if="getAllPhotos" class="guradian">
+    <div class="photo__container" v-for="photo in getAllPhotos" :key="photo._id">
+      <img class="photo__container--photo" :src="photo.imageUrl" :alt="photo.title" />
+
+      <div class="photo__details">
+        <p class="photo__details--likes">{{photo.likes}} likes</p>
+        <h4 class="photo__details--heading">{{photo.title}}</h4>
+        <p class="photo__details--description">{{photo.description}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// import { gql } from 'apollo-boost';
+import { gql } from "apollo-boost";
 
 export default {
-  name: 'Home'
+  name: "Home",
+  apollo: {
+    getAllPhotos: {
+      query: gql`
+        query {
+          getAllPhotos {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
+    }
+  }
 };
 </script>
+
+<style lang="scss">
+.guradian {
+  background-color: rgb(240, 233, 233);
+  border: 1px solid #c5c2c2;
+  margin-top: 5rem;
+}
+
+.photo__container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 4rem;
+
+  &--photo {
+    width: 100%;
+  }
+}
+
+.photo__details {
+  padding: 1.5rem 2rem;
+
+  &--likes {
+    font-size: 1.4rem;
+    font-weight: 600;
+  }
+
+  &--heading {
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+
+  &--description {
+    font-size: 1.5rem;
+  }
+}
+</style>
